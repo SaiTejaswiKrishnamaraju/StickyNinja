a9f859966fdecd9089d65dc296bd609628311036
Mamallan Arumugam <MA00472778@techmahindra.com>
2018-01-30 11:29:37 +0530

    SN Flash Source file

---
+++ StickyNinjaMissions_CoolMath/CopyDataToUnfuddle.bat
@@ -0,0 +1,3 @@
+xcopy c:\Flash\StickyNinjaMissions\bin\runs.bin c:\OnlineRepositories\StickyNinjaMissions\bin /Y
+xcopy c:\Flash\StickyNinjaMissions\bin\*.xml c:\OnlineRepositories\StickyNinjaMissions\bin /Y
+xcopy c:\Flash\StickyNinjaMissions\*.as3proj c:\OnlineRepositories\StickyNinjaMissions /Y
